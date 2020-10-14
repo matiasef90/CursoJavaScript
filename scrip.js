59ceed4b3017435333fbd0e28869e1ca6670f655
Matías Fernandez <matiasef90@gmail.com>
2020-10-13 22:37:56 -0300

    Desafio 3 Finalizado.-

--- scrip.js
+++ scrip.js
@@ -1,25 +1,22 @@
 var semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
+/*Informa si el dia de la semana es par o impar. 
+En el caso de que sea el día 7 de la semana imprime
+ una alerta por pantalla.-*/
 
-console.log(semana.length);
-
-for(var i; i < semana.length; i++){
-    console.log(semana[i]);
-}
-
-function informarDiasParesImpar(dias){
-    for(var i; i < dias.length; i++){
+function informarDiasParesImpar(dias) {
+    for (var i = 0; i < dias.length; i++) {
         var num = 0;
         num = i + 1;
-        if(num % 2 == 0){
+        if (num % 2 == 0) {
             console.log(dias[i] + " es un día par.-");
-        }else{
+        } else {
             console.log(dias[i] + " es un día impar.-");
         }
-        if(num == 7){
+        if (num == 7) {
             alert("Hoy es Domingo!!");
         }
     }
 }
 
-diasPares(semana);
+informarDiasParesImpar(semana);
